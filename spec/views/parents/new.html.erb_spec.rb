2c1ff91dfb4790ec45197a6d7668f1f3c582c079
Robert Balderas <robert@rbdevelop.com>
2015-05-01 14:51:52 -0500

    user models started

---
+++ spec/views/parents/new.html.erb_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe "parents/new", type: :view do
+  before(:each) do
+    assign(:parent, Parent.new(
+      :first_name => "MyString",
+      :last_name => "MyString"
+    ))
+  end
+
+  it "renders new parent form" do
+    render
+
+    assert_select "form[action=?][method=?]", parents_path, "post" do
+
+      assert_select "input#parent_first_name[name=?]", "parent[first_name]"
+
+      assert_select "input#parent_last_name[name=?]", "parent[last_name]"
+    end
+  end
+end
---
+++ spec/views/parents/show.html.erb_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe "parents/show", type: :view do
+  before(:each) do
+    @parent = assign(:parent, Parent.create!(
+      :first_name => "First Name",
+      :last_name => "Last Name"
+    ))
+  end
+
+  it "renders attributes in <p>" do
+    render
+    expect(rendered).to match(/First Name/)
+    expect(rendered).to match(/Last Name/)
+  end
+end
