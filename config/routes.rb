40d36d8209edc26bfcf319365d0bc43b001cc998
Robert Balderas <robert@rbdevelop.com>
2015-05-04 08:29:01 -0500

    pushing model changes and devise updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :students
+
   devise_for :parents
   resources :parents
 
---
+++ spec/views/students/new.html.erb_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe "students/new", type: :view do
+  before(:each) do
+    assign(:student, Student.new(
+      :first_name => "MyString",
+      :last_name => "MyString",
+      :grade => 1
+    ))
+  end
+
+  it "renders new student form" do
+    render
+
+    assert_select "form[action=?][method=?]", students_path, "post" do
+
+      assert_select "input#student_first_name[name=?]", "student[first_name]"
+
+      assert_select "input#student_last_name[name=?]", "student[last_name]"
+
+      assert_select "input#student_grade[name=?]", "student[grade]"
+    end
+  end
+end
